39757d770c8bf5c3500cd14554964ea310cabba4
harshsakariya <harshsakariya115@gmail.com>
2022-02-06 15:26:42 +0530

    Adding joke controller. closes #5

---
+++ src/main/java/com/cobra/springbootjokesapp/controller/JokeController.java
@@ -0,0 +1,23 @@
+package com.cobra.springbootjokesapp.controller;
+
+import com.cobra.springbootjokesapp.serivces.JokeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+
+    private final JokeService jokeService;
+
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model){
+        model.addAttribute("joke", this.jokeService.getJoke());
+        return "index";
+    }
+
+}
