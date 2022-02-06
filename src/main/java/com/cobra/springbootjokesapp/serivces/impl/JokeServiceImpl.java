a363ad055c674064b976d031775f6f12baa00439
harshsakariya <harshsakariya115@gmail.com>
2022-02-06 15:09:49 +0530

    Adding joke service. closes #3

---
+++ src/main/java/com/cobra/springbootjokesapp/serivces/impl/JokeServiceImpl.java
@@ -0,0 +1,21 @@
+package com.cobra.springbootjokesapp.serivces.impl;
+
+import com.cobra.springbootjokesapp.serivces.JokeService;
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JokeServiceImpl implements JokeService {
+
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokeServiceImpl(){
+        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    }
+
+    @Override
+    public String getJoke() {
+        return this.chuckNorrisQuotes.getRandomQuote();
+    }
+
+}
